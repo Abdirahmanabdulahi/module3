25d36db6dd9caeea5ed0db18faf674849c4aedb6
wardheere <wardheere388@gmail.com>
2024-06-06 08:32:46 +0300

    Added Upper\&Slice\&Reverse.py

---
+++ Upper&Slice&Reverse.py
@@ -0,0 +1,18 @@
+
+
+name= "jaamac geedi"
+print(name.upper())
+
+
+
+ 
+namevlue1=name[0:6]
+
+namevlue2=name[7:12]
+
+nameval3= namevlue2+" "+ namevlue1
+print(nameval3)
+
+
+name= num_reverse = name [::-1]
+print(name)
