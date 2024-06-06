5c675b3384d3bf46aae5180baa31817ac9262490
wardheere <wardheere388@gmail.com>
2024-06-06 12:32:50 +0300

    Added set.py

---
+++ set.py
@@ -0,0 +1,46 @@
+
+
+
+main_set = {"Yoonis","Faysal"  , "Ahmed" }
+print(main_set)
+print()
+
+
+mini_set = { "Anas","Xamza" ,"Ahmed" }
+print(mini_set)
+print()
+
+
+Totl_mains = main_set.union(mini_set)
+print(Totl_mains )
+print()
+
+
+print(main_set.difference(mini_set) )
+print()
+
+
+
+print(main_set.symmetric_difference(mini_set) )
+print()
+
+
+
+print(main_set.intersection(mini_set) )
+print()
+
+
+
+Totl_mains.add("nuur")
+print(Totl_mains )
+print()
+
+
+print(mini_set-main_set) 
+
+#union
+#deffirence
+#symmetric_deffirence
+#intersection
+#discard
+
