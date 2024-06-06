85c16a5c2435b05d036f3f1c6908c96e23783247
wardheere <wardheere388@gmail.com>
2024-06-06 12:49:29 +0300

    Added Key&Value.py

---
+++ Key&Value.py
@@ -0,0 +1,30 @@
+
+
+teachers = {10: "guuleed", 11: "cilmi", 12: "cawaale"}
+print(teachers)
+
+print()
+
+teachers[13]= "guure"  
+
+print(teachers)
+
+
+print()
+
+print(teachers[11])
+
+print()
+
+print(teachers.keys())
+
+print()
+
+print(teachers.values())
+
+
+print()
+
+print(teachers.popitem())
+
+
